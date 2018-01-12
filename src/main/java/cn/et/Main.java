a492692b53f182349f37cb13aaddd49d9270f44b
Peter <Peter@Peter>
2018-01-12 10:38:21 +0800

    提交

---
+++ src/main/java/cn/et/Main.java
@@ -0,0 +1,15 @@
+package cn.et;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+//开启服务注册中心
+@EnableEurekaServer
+public class Main {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+	}
+}
